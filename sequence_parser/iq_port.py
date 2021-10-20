19af49587b55199bec9f7262ea36bcb1bf6b40e6
yoshi74ls181 <yoshi74ls181@gmail.com>
2021-10-20 13:42:37 +0900

    Create `IQPort` to support compensation of mixer imbalances

---
+++ sequence_parser/iq_port.py
@@ -0,0 +1,64 @@
+import copy
+
+import numpy as np
+
+from .instruction.acquire import Acquire
+from .instruction.pulse.pulse import Pulse
+from .port import Port
+
+
+class IQPort(Port):
+    """A Port which compensates for the amplitude and delay imbalances of an IQ mixer"""
+
+    def __init__(self, name, if_freq=0.25,
+        i_factor=lambda freq: 1, q_factor=lambda freq: 1,
+        i_delay=lambda freq: 0, q_delay=lambda freq: 0
+    ):
+        """initial setting of the Port
+        Args:
+            name (str): port name
+            if_freq (float [GHz]): IF frequency
+            i_factor (freq [GHz] -> float): multiply I by this factor
+            q_factor (freq [GHz] -> float): multiply Q by this factor
+            i_delay (freq [GHz] -> float [ns]): delay I by this much
+            q_delay (freq [GHz] -> float [ns]): delay Q by this much
+        """
+        super().__init__(name, if_freq)
+        self.i_factor = i_factor
+        self.q_factor = q_factor
+        self.i_delay = i_delay
+        self.q_delay = q_delay
+
+    def _write_waveform(self, waveform_length):
+        """Write waveform by the Pulse instructions
+        Args:
+            waveform_length (float): total waveform time length
+        """
+        self.time = np.arange(0, waveform_length, self.DAC_STEP)
+        i_waveform = np.zeros(self.time.size, dtype=np.complex128)
+        q_waveform = np.zeros_like(i_waveform)
+        for instruction in self.syncronized_instruction_list:
+            if isinstance(instruction, Pulse):
+                # get the compensation parameters at the IF frequency of the pulse
+                if_freq = self.if_freq + self.detuning
+                i_factor = self.i_factor(if_freq)
+                q_factor = self.q_factor(if_freq)
+                i_delay = self.i_delay(if_freq)
+                q_delay = self.q_delay(if_freq)
+
+                # compensate and write the I waveform
+                i_pulse = copy.deepcopy(instruction)
+                i_pulse.pulse_shape.amplitude *= i_factor
+                i_pulse.position += i_delay
+                i_pulse._write(self, out=i_waveform)
+
+                # compensate and write the Q waveform
+                q_pulse = copy.deepcopy(instruction)
+                q_pulse.pulse_shape.amplitude *= q_factor
+                q_pulse.position += q_delay
+                q_pulse._write(self, out=q_waveform)
+
+            elif isinstance(instruction, Acquire):
+                instruction._acquire(self)
+
+        self.waveform = i_waveform.real + 1j * q_waveform.imag
