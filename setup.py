2e1d5d304230c7a1ba97a2789f960a5085dbdcfa
yoshi74ls181 <yoshi74ls181@gmail.com>
2023-02-21 10:01:49 +0900

    Automatically install dependencies on `pip install`

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(name="sequence-parser", packages=["sequence_parser"])
+    setup(
+        name="sequence-parser",
+        packages=["sequence_parser"],
+        install_requires=[
+            "numpy",
+            "matplotlib",
+            "networkx",
+        ]
+    )
